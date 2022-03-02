f817e6bc1c915c7829f81eb0e434d34f4985ab5f
Chinmayeegade <chinmayeepgade@gmail.com>
2022-03-02 20:03:04 +0530

    Created

--- rea_ct/src/fire.js
+++ rea_ct/src/fire.js
@@ -3,12 +3,12 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
  var firebaseConfig = {
-   apiKey: "AIzaSyCYkBcoqmErWSuMnYNboF8r5_PcSESverE",
-   authDomain: "fir-7b6ef.firebaseapp.com",
-   projectId: "fir-7b6ef",
-   storageBucket: "fir-7b6ef.appspot.com",
-   messagingSenderId: "102846281637",
-   appId: "1:102846281637:web:b444aa0aab4d2774de5a4c"
+    apiKey: "*******************************",
+    authDomain: "********************",
+    projectId: "***************",
+    storageBucket: "**********",
+    messagingSenderId: "********",
+    appId: "**********************"
  };
 
  const fire = firebase.initializeApp(firebaseConfig);
